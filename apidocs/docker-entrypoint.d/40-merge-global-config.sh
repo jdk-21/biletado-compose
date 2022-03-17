3dd773dd1afa99a40a3493e80897c28d4c00a786
Matthias Blümel <blaimi@blaimi.de>
2022-03-17 18:58:29 +0100

    make apidocs global-config valid openapi-doc

--- apidocs/docker-entrypoint.d/40-merge-global-config.sh
+++ apidocs/docker-entrypoint.d/40-merge-global-config.sh
@@ -3,5 +3,5 @@
 apk --no-cache add yq
 
 for file in /usr/share/nginx/html/openapi/*.yaml; do
-  yq -i eval-all 'select(fileIndex == 0) * select(filename == "/tmp/global-config.yaml")' ${file} /tmp/global-config.yaml
+  yq -i eval-all 'select(filename == "/tmp/global-config.yaml") * select(fileIndex == 0)' ${file} /tmp/global-config.yaml
 done
